8a6a5e36597faff7e3fb9066f9acdceb7c4e4b0f
Vanessa Telles <tellesvanessa07@gmail.com>
2018-03-28 02:51:17 -0300

    begin

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+'use strict';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return <Text style={styles.description}>Search for houses to buy!</Text>;
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
